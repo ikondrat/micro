4459eba32fc5f6941774b70e486a2440c7d2e97e
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2017-11-21 10:53:23 +0100

    fix

--- index.js
+++ index.js
@@ -2,29 +2,35 @@
 const http = require('http'),
       fs = require('fs'),
       hostname = '0.0.0.0',
-      portHttp = '80';
+      portHttp = '8080';
       
 const routeRequest = (req, res) => {
   res.statusCode = 200;
-  let fileName = req.url;
+  let fileName = req.url, itemId = null;
   if (fileName === '/' || fileName === '/itineraries/') {
     fileName = '/index.html';
   }
-  
+
+  if (fileName.match(/\/api\/movies/) ) {
+    let parts = fileName.split('/');
+    itemId = parts.length === 4 ? fileName.split('/').pop() : null;
+    fileName = '/api/movies/index.json';
+  }
+
   let contentType = "text/html";
 
   if (fileName.match(/\.js/)) {
     contentType = "text/javascript";
   }
 
-  if (fileName.match(/\.json/)) {
+  if (fileName.match(/\.json/) || fileName.match(/\/api/)) {
     contentType = "application/json";
   }
 
   if (fileName.match(/\.css/)) {
     contentType = "text/css";
   }
-  fs.readFile('.' + fileName, function (err, html) {
+  fs.readFile('.' + fileName, 'utf8', function (err, data) {
     const code = err ? 404 : 200;
     if (err) {
         // throw err; 
@@ -34,8 +40,16 @@ const routeRequest = (req, res) => {
       res.writeHeader(code, {
         "Content-Type": contentType,
         "Access-Control-Allow-Origin": "*"
-      });  
-      res.write(html);  
+      });
+      if (itemId) {
+        let obj = JSON.parse(data);
+        res.write(
+          JSON.stringify(obj.find(obj => obj.id === parseInt(itemId)))
+        );  
+      } else {
+        res.write(data);  
+      }
+      
     }
     console.log(`${code} request: ${fileName}`);
     res.end();  
